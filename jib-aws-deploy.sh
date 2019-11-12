ca91065815a985cbc97b8607d254d2b8c02d55fe
Mazunin Konstantin <mazuninky@gmail.com>
2019-11-12 05:55:03 +0300

    Add possibility to omit buldParams for jib-aws-deploy

--- jib-aws-deploy.sh
+++ jib-aws-deploy.sh
@@ -9,27 +9,33 @@
 # * aws-cli
 #
 # Usage:
-#  $ ./jib-aws-deploy needToLogin buildParams serviceANR clusterANR checkPeriod
+#  $ ./jib-aws-deploy needToLogin serviceANR clusterANR checkPeriod [buildParams]
 # * needToLogin: contains yes if need to login before push into Docker registry
-# * buildParams: comma-separated build options
 # * serviceANR: anr link to the service
 # * clusterANR: anr link to the cluster
 # * checkPeriod: health check grace period in seconds for the service
+# * buildParams: comma-separated build options
 
-if [ $1 -eq "yes"]; then
-    $(aws ecr get-login --no-include-email --region eu-central-1)
-    if [ $? -eq 1 ]; then
-        echo "An error occurred while logging in to aws"
-        exit 1
-    fi
+if [ "$1" = "yes" ]; then
+  $(aws ecr get-login --no-include-email --region eu-central-1)
+  if [ $? -eq 1 ]; then
+    echo "An error occurred while logging in to aws"
+    exit 1
+  fi
 fi
 
-if ! ./gradlew -P$2 jib; then
+if [ -z "$5" ]; then
+  ./gradlew jib
+else
+  ./gradlew -P$5 jib
+fi
+
+if [ $? -eq 1 ]; then
   echo "An error occurred while building the Docker image"
   exit 2
 fi
 
-if ! aws ecs update-service --cluster $3 --service $4 --health-check-grace-period-seconds $5 --force-new-deployment; then
+if ! aws ecs update-service --cluster "$2" --service "$3" --health-check-grace-period-seconds "$4" --force-new-deployment; then
   echo "An error occurred while restarting the service"
   exit 3
 fi
