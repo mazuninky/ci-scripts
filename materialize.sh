f2e01c611f2f38350fc686702bcc98d11329e50b
Mazunin Konstantin <mazuninky@gmail.com>
2019-11-12 04:53:09 +0300

    Add meterialize script

---
+++ materialize.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#=============#
+# Materialize #
+#=============#
+# Tool for decoding base64 string from param into a file
+#
+# Usage:
+#  $ ./materialize inputString filePath
+# * inputString: base64 encoded string
+# * filePath: path to the file to write to
+# Example:
+#  $ ./materialize $SECRET_FILE app/secrets/secret.txt
+
+echo "$1" | base64 --decode > "$2"
